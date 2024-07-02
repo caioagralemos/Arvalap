e213c1b578fd83f55541df92701648f0d074e55a
Caio <caioagralemos@live.com>
2024-07-02 11:31:52 -0300

    Initial Commit

---
+++ Arvalap/ArvalapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArvalapApp.swift
+//  Arvalap
+//
+//  Created by Caio on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ArvalapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
